2601805a7ce836a860994fa04160127ff7d269df
Alexis Lopez Zubieta <azubieta90@gmail.com>
2016-06-28 23:07:07 -0400

    Include missing file.

---
+++ plasmoid/contents/code/data.js
@@ -0,0 +1,38 @@
+var runnersSelection = [
+    [""],
+    ["services", "shell"],
+    ["places","locations", "folder", "baloosearch"], 
+    ["calculator", "unitconverter"],
+    ["bookmarks"]
+]
+
+var runnersModels = [];
+var currentRunner = null;
+
+function initRunnerModels(parent, query) {
+    for (var i = 0; i < 5; i++) {
+        var newRunner = Qt.createQmlObject(
+                    'import org.kde.runnermodel 2.0; RunnerModel {}',
+                    parent)
+        
+        newRunner.query = query;
+        print(runnersSelection[i])
+        newRunner.runners = runnersSelection[i];
+        runnersModels[i] = newRunner;
+    }
+}
+
+function changeListModel(listView, modelIndex) {
+    currentRunner = runnersModels[modelIndex]; 
+    updateQuery("");
+    listView.model =  currentRunner;
+}
+
+function updateQuery(query){
+    currentRunner.query = query;
+}
+
+function triggerAction(idx) {
+    currentRunner.run(idx);
+}
+
